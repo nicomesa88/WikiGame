c41bca7f770a2deedd661e807b2eb6bf5fff3fdc
Nicolas Mesa <nicolasmesa@Nicolass-MacBook-Air.local>
2016-07-26 11:25:57 -0500

    home view file created, routes in app.js created

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,11 +5,34 @@ import init from './init'
 
 
 const app = function() {
-  document.querySelector('.container').innerHTML = "<h1>Woah!</h1>"
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            'home':'handleHome',
+            'game/play':'handleLiveGame',
+            '*cathAll': 'redirectHome'
+        },
+
+        handleHome: function () {
+            ReactDOM.render( <Home/>, document.querySelector('.container'))
+        },
+
+        handleLiveGame: function () {
+            ReactDOM.render( <GameView />, document.querySelector('.container'))
+        },
+
+        redirectHome: function () {
+            location.hash = 'home'
+        },
+
+        initialize: function(){
+            Backbone.history.start()
+        }
+    })
+    new AppRouter()
 }
 
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
-// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE. 
+// NECESSARY FOR USER FUNCTIONALITY. DO NOT CHANGE.
 export const app_name = init()
 app()
 // x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..x..
